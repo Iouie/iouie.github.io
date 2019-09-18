16b93685f87439cf1d0b99b7a539c3d6145199a0
Stanley Louie <sxl7953@rit.edu>
2019-09-18 19:52:22 -0400

    phpform

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n".
+"User Message: $message.\n";
+
+
+$to = "sxl7953@rit.edu";
+
+$headers = "From: $visitor_email \r\n";
+
+$headers .= "Reply To: $visitor_email \r\n";
+
+mail($to,email_subject,$email_body,$headers);
+
+header("Location: iouie.github.io");
+
+
+?>
